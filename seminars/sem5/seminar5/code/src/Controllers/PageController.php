8eeebad0e913cff031d081f10ef848a524bfedb2
NataliyaYadykina <nataliya.yadykina@yandex.ru>
2024-07-29 05:34:13 -0900

    added sem_5

---
+++ seminars/sem5/seminar5/code/src/Controllers/PageController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Geekbrains\Application1\Controllers;
+
+use Geekbrains\Application1\Application;
+use Geekbrains\Application1\Render;
+
+class PageController
+{
+
+    public function actionIndex()
+    {
+        $render = new Render();
+        // echo Application::config()['storage']['address'];
+        return $render->renderPage('page-index.twig', ['title' => 'Главная страница']);
+    }
+}
