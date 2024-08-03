f8d3cb22f2cd9417402ef7b66b7a7a310e6c2ae6
NataliyaYadykina <nataliya.yadykina@yandex.ru>
2024-08-03 08:46:34 -0900

    added les_7

---
+++ lessons/les7/code/src/Application/Render.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Geekbrains\Application1\Application;
+
+use Twig\Loader\FilesystemLoader;
+use Twig\Environment;
+
+class Render
+{
+
+    private string $viewFolder = '/Domain/Views/';
+    private FilesystemLoader $loader;
+    private Environment $environment;
+
+
+    public function __construct()
+    {
+
+        $this->loader = new FilesystemLoader(dirname(__DIR__) . $this->viewFolder);
+        $this->environment = new Environment($this->loader, [
+            // 'cache' => $_SERVER['DOCUMENT_ROOT'].'/cache/',
+        ]);
+    }
+
+    public function renderPage(string $contentTemplateName = 'page-index.twig', array $templateVariables = [])
+    {
+        $template = $this->environment->load('layouts/main.twig');
+
+        $templateVariables['content_template_name'] = $contentTemplateName;
+        $templateVariables['title'] = 'Главная страница';
+        
+        if (isset($_SESSION['user_name'])) {
+            $templateVariables['user_authorized'] = true;
+        }
+
+        return $template->render($templateVariables);
+    }
+    
+    public function renderPageWithForm(string $contentTemplateName =
+    'page-index.twig', array $templateVariables = [])
+    {
+        $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+        $templateVariables['csrf_token'] = $_SESSION['csrf_token'];
+        return $this->renderPage($contentTemplateName, $templateVariables);
+    }
+}
